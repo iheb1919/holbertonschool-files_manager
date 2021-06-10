49df22ddaf96195efce372628c0da1e8ffcad71c
iheb1919 <ihebmejri14@gmail.com>
2021-06-10 17:25:52 +0200

    m

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.asyncGet = promisify(this.client.get).bind(this.client);
+    this.client.on('error', function(error) {
+        console.log(`Redis client not connected to the server: ${error}`);
+    });
+ 
+  }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+  async get(key) {
+    
+    const value = await this.asyncGet(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
