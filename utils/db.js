516c0ffb3bba00518a698d69d29aba59c84ffaca
iheb1919 <ihebmejri14@gmail.com>
2021-06-12 01:41:25 +0200

    try

--- utils/db.js
+++ utils/db.js
@@ -1,19 +1,19 @@
 const { MongoClient } = require('mongodb');
 
-const host = process.env.DB_HOST || 'localhost';
-const port = process.env.DB_PORT || 27017;
-const database = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${host}:${port}`;
-
 class DBClient {
   constructor() {
-    MongoClient.connect(url, (err, client) => {
-      if (!err) {
-        this.db = client.db(database);
-      } else {
-        this.db = false;
-      }
-    });
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+    MongoClient(url, { useUnifiedTopology: true })
+      .connect()
+      .then(async (client) => {
+        this.client = client;
+        this.connected = true;
+        this.db = this.client.db(database);
+      })
+      .catch(console.error);
   }
 
   isAlive() {
